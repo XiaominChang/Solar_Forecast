2ad65923ba8d4719bb46f939b966b112459913a8
XiaominChang <33690769+XiaominChang@users.noreply.github.com>
2019-08-22 14:40:05 +1000

    mo

--- scratches/RNNtrain.py
+++ scratches/RNNtrain.py
@@ -4,12 +4,14 @@ import csv
 import matplotlib.pyplot as plt
 from math import sqrt
 from tensorflow import keras
+import os
 # model itself
 #from keras.models import Sequential
 #from keras.layers import LSTM
 #from keras.layers import Dense
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 def dataReader():
     file=open("C:/Users/Xiaoming/Desktop/trainning_data/SolarPrediction.csv/SolarPrediction.csv", 'r', encoding='utf-8' )
     reader=csv.reader(file)
@@ -17,8 +19,8 @@ def dataReader():
     output=[]
     for row in reader:
         feature=[]
-        feature.append(row[1])
-        feature.append(row[2])
+        #feature.append(row[1])
+        #feature.append(row[2])
         feature.append(float(row[3]))
         feature.append(float(row[4]))
         feature.append(float(row[5]))
@@ -46,17 +48,17 @@ def dataReader():
     i=16237
     while(i>7416):
         oct_in.append(features[i])
-        oct_out.append(features[i])
+        oct_out.append(output[i])
         i-=1
     i=24521
     while(i>16237):
         Nov_in.append(features[i])
-        Nov_out.append(features[i])
+        Nov_out.append(output[i])
         i-=1
     i=len(features)-1
     while(i>24521):
         Dec_in.append(features[i])
-        Dec_out.append(features[i])
+        Dec_out.append(output[i])
         i-=1
     input=sep_in + oct_in + Nov_in + Dec_in
     output=sep_out+ oct_out +Nov_out+ Dec_out
@@ -77,6 +79,8 @@ def sequence( n_steps):
         seq_x, seq_y= x[i:end, :], y[end]
         input.append(seq_x)
         output.append(seq_y)
+    print(np.shape(input))
+    print(np.shape(output))
     return np.array(input), np.array(output)
 
 def LSTM_training():
@@ -84,12 +88,33 @@ def LSTM_training():
     X, y= sequence(n_steps)
     train_X, train_y=X[:-1000,:],y[:-1000]
     test_X, test_y=X[-1000:,:], y[-1000:]
+    #input, out=X[8002:8003,:],y[8002:8003]
+    #train_X=np.array(input)
+    #train_y=np.array(out)
+
+    print(train_X.dtype)
+    print(train_y.dtype)
+    print(train_y)
+    print(np.shape(train_X))
+    print(np.shape(train_y))
     model=keras.models.Sequential()
     model.add(keras.layers.LSTM(300, activation='relu', return_sequences=True, input_shape=(n_steps, 6)))
     model.add(keras.layers.LSTM(200, activation='relu'))
     model.add(keras.layers.Dense(1))
     model.compile(optimizer='adam', loss='mse')
-    model.fit(train_X,train_y, epochs=10)
+    print('start training')
+    model.fit(train_X,train_y, epochs=1)
+    model.save('C:/Users/Xiaoming/Desktop/trainning_data/LSTM.h5')
+
+LSTM_training()
+def test():
+    X, y= sequence(10)
+    i=0
+    while (i<32670):
+        shape=np.shape(X[i])
+        if(shape != (10,6)):
+            print(shape)
+            print(i)
 
 
 
